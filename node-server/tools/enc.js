cfdc0ef401d5165954b5ea5c617037ef4b6c65d5
x <917440285@qq.com>
2018-05-27 22:39:00 +0800

    second

--- node-server/tools/enc.js
+++ node-server/tools/enc.js
@@ -15,14 +15,3 @@ var smallenc=text=>{
 
 exports.smallenc=smallenc;
 
-
-
-
-//  var crtpto=require("crypto");
-
-//  var enc=text=>{
-//      var md5=crypto.createHash("md5");
-//      md5.update(text);
-//      return md5.digest("hex");
-//  }
-//   module.exports=enc
